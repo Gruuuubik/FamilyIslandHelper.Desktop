c65e2b190e9732f1c2cb77e5063774b2a8751119
Pavel Zhuravlevich <p.zhuravlevich@gmail.com>
2023-09-03 13:46:45 +0200

    Add ShamanWorkshop

---
+++ FamilyIslandHelper.Api/Models/Buildings/ShamanWorkshop.cs
@@ -0,0 +1,16 @@
+﻿using FamilyIslandHelper.Api.Models.Abstract;
+using FamilyIslandHelper.Api.Models.Items;
+using System.Collections.Generic;
+
+namespace FamilyIslandHelper.Api.Models.Buildings
+{
+	public class ShamanWorkshop : Building
+	{
+		public override string Name => "Мастерская шамана";
+		public override double ProduceRatio => 1;
+		public override List<ProducibleItem> Items => new List<ProducibleItem>
+		{
+			new SapphireBracelet()
+		};
+	}
+}
---
+++ FamilyIslandHelper.Api/Models/Items/ShamanWorkshopItems.cs
@@ -0,0 +1,22 @@
+﻿using FamilyIslandHelper.Api.Models.Abstract;
+using FamilyIslandHelper.Api.Models.Buildings;
+using System;
+using System.Collections.Generic;
+
+namespace FamilyIslandHelper.Api.Models.Items
+{
+	public class RuneStone : ProducibleItem
+	{
+		public override string Name => "Рунный камень";
+		public override int LevelWhenAppears => 62;
+		public override TimeSpan OriginalProduceTime => TimeSpan.FromHours(6);
+		public override Building BuildingToCreate => new ShamanWorkshop();
+
+		public override List<(Item item, int count)> Components => new List<(Item item, int count)>
+			{
+				(new StoneBlock(), 3),
+				(new Knife(), 5),
+				(new BluePaint(), 1),
+			};
+	}
+}
